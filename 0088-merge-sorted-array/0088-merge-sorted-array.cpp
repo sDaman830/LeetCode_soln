0b71dc9d7c581e5ca63c58679315f1a8b58ace34
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-06-06 08:45:01 +0530

    Time: 0 ms (100.00%), Space: 9 MB (51.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -16,7 +16,7 @@ public:
             k--;
         }
         
-        // Copy remaining elements from nums2 to nums1 if any
+     
         while (j >= 0) {
             nums1[k] = nums2[j];
             j--;
