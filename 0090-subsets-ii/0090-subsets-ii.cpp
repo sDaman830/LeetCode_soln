82a37a5dbb7bdbcfeed638c4f3d30981204e3d8c
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-06-14 08:42:34 +0530

    Time: 8 ms (28.80%), Space: 8.7 MB (30.46%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -3,24 +3,35 @@
 
 class Solution {
 public:
-    void subsetsWithDupHelper(vector<int>& nums, int start, vector<int>& subset, vector<vector<int>>& result) {
-        result.push_back(subset); 
+    void subsetsWithDupHelper(vector<int>& nums, int i, vector<int>& subset, vector<vector<int>>& result,int n) {
+       if(i== n)
+       {
+           result.push_back(subset);
+           return;
+}
+        subset.push_back(nums[i]);
+        subsetsWithDupHelper(nums, i+1, subset,  result,n);
+        subset.pop_back();
+         subsetsWithDupHelper(nums,  i+1, subset,  result,n);
         
-        for (int i = start; i < nums.size(); i++) {
-            if (i > start && nums[i] == nums[i - 1])
-                continue; 
-            
-            subset.push_back(nums[i]); 
-            subsetsWithDupHelper(nums, i + 1, subset, result); 
-            subset.pop_back(); 
-        }
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> result;
         vector<int> subset;
+        int n = nums.size();
         sort(nums.begin(), nums.end()); 
-        subsetsWithDupHelper(nums, 0, subset, result);
-        return result;
+        subsetsWithDupHelper(nums, 0, subset, result,n);
+        map<vector<int>,int> mp;
+        for(auto i : result)
+        {
+            mp[i]++;
+}
+        vector<vector<int>> ans;
+        for(auto j: mp)
+        {
+            ans.push_back(j.first);
+}
+        return ans;
     }
 };
