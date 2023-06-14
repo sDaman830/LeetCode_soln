2fcdd452a6f0a409db3f2c601d7b8b2b76435c80
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-06-14 16:52:38 +0530

    Time: 18 ms (33.90%), Space: 13.7 MB (40.06%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+  void   sam(int target , int i , vector<int> &c, vector<vector<int>> &ans,vector<int> &su,int n )
+    {
+        
+        if(target == 0)
+        {
+            ans.push_back(su);
+         
+}
+           if(target<0) return;
+      if(i==n) return;
+        su.push_back(c[i]);
+        sam(target-c[i],i,c,ans,su,n);
+            su.pop_back();
+        sam(target,i+1,c,ans,su,n);
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> su;
+        sam(target,0,candidates,ans,su,candidates.size());
+       map<vector<int>,int> mp;
+        for(auto i: ans)
+            mp[i]++;
+        vector<vector<int>> a;
+        for(auto i : mp)
+            a.push_back(i.first);
+        return a;
+            
+        
+    }
+};
