7ebdebb824349570b1fc86601274c659d1eabb2b
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-08-01 16:39:17 +0530

    Time: 19 ms (87.17%), Space: 17.9 MB (31.99%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void bb(int n , int k , vector<vector<int>>& ans ,int na,vector<int> &subans)
+    {
+        if(!k) 
+        {
+            ans.push_back(subans);
+            return;
+        }
+        if(na == n+1) return;
+        
+        bb(n , k , ans , na+1 ,subans);
+        subans.push_back(na);
+         
+         bb(n,k-1,ans,na+1,subans);
+        
+       subans.pop_back();
+
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> subans;
+        bb(n , k , ans,1,subans);
+        return ans;
+        
+    }
+};
