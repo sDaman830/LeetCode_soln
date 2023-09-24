d85ea4cb3a27627b09f15199baa0ef46b93ab0d9
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-09-24 17:47:52 +0530

    Time: 31 ms (83.92%), Space: 35.8 MB (6.06%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -13,12 +13,18 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         hours=h;
         int left=1;
-        int right=1e9;
-        while(left<right){
+        int right=0;
+         if(check(piles,left)) return left;
+        for(auto it: piles)
+        {
+            right = max(it,right);
+        }
+        while(right>left+1){
             int mid=left + (right-left)/2;
             if(check(piles,mid)) right=mid;
-            else left=mid+1;
+            else left=mid;
         }
-        return left;
+       
+        return right;
     }
 };
